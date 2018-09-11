716adfd91586829363a0469dfe5aeb65f4b1ed7e
Sagurov Anton <sagurovanton@gmail.com>
2018-09-11 16:00:49 +0300

    Запускающий скрипт
    TODO: молчаливый режим ввести
    TODO: добавить расчетчик времени сканировани

---
+++ lanscan.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+
+import time
+from core import lanscan
+from wrapper import get_arguments
+
+if __name__ == "__main__":
+
+    address_pool = get_arguments()
+    address_amount = len(address_pool)
+    start_time = time.time()
+
+    alive_hosts, dead_hosts, other_errors = lanscan(address_pool, step=200, timeout=2)
+
+    print('\nЗатрачено времени:', round(time.time() - start_time, 3), 'секунд')
+    print('Адресов просканировано:', address_amount)
+    print('Включенных хостов: ', len(alive_hosts))
+    print('Выключенных хостов:', len(dead_hosts))
+    print('Ответов с ошибками:', len(other_errors))
+
+    #TODO: молчаливый режим ввести
+
+    if len(other_errors) != 0:
+        for error in other_errors:
+            print(error)
